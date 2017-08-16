944cda651cf6ac727c4d19ee5c6bcf085197e7bb
Sumppa <asd@asdasdasdasd.fi>
2017-08-16 13:02:45 +0300

    Sync

--- 2D-ARPG/Game/Game1.cs
+++ 2D-ARPG/Game/Game1.cs
@@ -1,5 +1,4 @@
-﻿
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
@@ -192,7 +191,6 @@ namespace _2D_ARPG
             player.Update(gameTime);
             if (worldmap == 1)
             {
-
                 if (currentKeyboardState.IsKeyDown(Keys.A) && currentKeyboardState.IsKeyUp(Keys.W) && currentKeyboardState.IsKeyUp(Keys.S)
                 && currentKeyboardState.IsKeyUp(Keys.D))
                 {
