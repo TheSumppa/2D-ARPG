6d989b07844be6771bdf3e406434963b01b22eb2
Sumppa <asd@asdasdasdasd.fi>
2017-08-16 14:19:10 +0300

    Collision and graphics

--- 2D-ARPG/Game/Game1.cs
+++ 2D-ARPG/Game/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
 using System;
 using System.Diagnostics;
 using System.Xml.Linq;
@@ -11,17 +13,17 @@ namespace _2D_ARPG
     /// This is the main type for your game.
     public class Game1 : Game
     {
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+        GraphicsDeviceManager graphics;             // Our GraphicsManager
+        SpriteBatch spriteBatch;                    // Spritebatch we use to draw stuff
         Player player;                              // Player class
         KeyboardState currentKeyboardState;         // Current Keyboardstate used in movement
         KeyboardState previousKeyboardState;        // previous Keyboardstate used in movement
         Tile[,] tileset;                            // Multidimensional array for tiles
         float playerMoveSpeed = 16;                 // Player movespeed
-        int worldmap = 0;                           // Variable used for drawing woldmap
+        int worldMap = 0;                           // Variable used for drawing worldmap
         float keyRepeatTime;                        // repeattime used for movement
         float elapsedTime;                          // Elapsed time used for movement
-        const float keyRepeatDelay = 0.33f;         // Repeat rate
+        float keyRepeatDelay = 0.25f;               // Repeat rate
         public SpriteFont font;                     // Sprite font used for text
         Camera camera;                              // Game camera
         int[,] CollisionIDs = new int[100, 100];    // ID's used for collision
@@ -46,6 +48,8 @@ namespace _2D_ARPG
         {
             // TODO: Add your initialization logic here
             player = new Player();                          // Initializing our player
+            player.Attack = 5;                              // Player Attack value
+            player.Life = 10;                               // Player Life value
             tileset = getTileset();                         // Initializing our tileset
             camera = new Camera(this.GraphicsDevice);       // Initializing our gameCamera
             base.Initialize();
@@ -102,14 +106,13 @@ namespace _2D_ARPG
             spriteBatch = new SpriteBatch(GraphicsDevice);
             WalkAnimation playerAnimation = new WalkAnimation();
             Texture2D playerTexture = Content.Load<Texture2D>("knightwalkanimation");
-            Vector2 playerPosition = new Vector2(768, 800);
+            Vector2 playerPosition = new Vector2(880, 768);
             Rectangle playerRectangle = new Rectangle((int)playerPosition.X, (int)playerPosition.Y, 16, 16);
             playerAnimation.Initialize(playerTexture, playerPosition, 16, 16, 2, 200, Color.White, 1.0f, true);
             player.Initialize(playerAnimation, playerPosition);
-            font = Content.Load<SpriteFont>("Text");
-            camera.Scale = 5.0f;                            // Camera "zoom" level
+            camera.Scale = 5.0f;
         }
-
+   
         /// UnloadContent will be called once per game and is the place to unload
         /// game-specific content.
         protected override void UnloadContent()
@@ -131,9 +134,10 @@ namespace _2D_ARPG
             this.camera.Update(gameTime);
             this.camera.Position = player.PlayerPosition;
             UpdatePlayer(gameTime);
+            Debug.WriteLine("Pos: " + player.PlayerPosition);
             base.Update(gameTime);
-
         }
+
         // To check if key is pressed
         bool KeyPressed(Keys key)
         {
@@ -146,7 +150,7 @@ namespace _2D_ARPG
             int tileValueUp = CollisionIDs[((int)player.PlayerPosition.X / 16 - 1), (((int)player.PlayerPosition.Y / 16 - 1)) - 1];
             if (tileValueUp == 1 || tileValueUp == 3 || tileValueUp == 5 || tileValueUp == 6 || tileValueUp == 7 || tileValueUp == 8 || tileValueUp == 9
                 || tileValueUp == 10 || tileValueUp == 11 || tileValueUp == 12 || tileValueUp == 14 || tileValueUp == 15 || tileValueUp == 16 || tileValueUp == 17
-                || tileValueUp == 19 || tileValueUp == 21 || tileValueUp == 22 || tileValueUp == 24 || tileValueUp == 29 || tileValueUp == 30 || tileValueUp == 31 || tileValueUp == 32
+                || tileValueUp == 19 || tileValueUp == 21 || tileValueUp == 22 || tileValueUp == 24 || tileValueUp == 28 || tileValueUp == 29 || tileValueUp == 30 || tileValueUp == 31 || tileValueUp == 32
                 || tileValueUp == 33 || tileValueUp == 34 || tileValueUp == 35 || tileValueUp == 36)
                 return true;
             return false;
@@ -157,7 +161,7 @@ namespace _2D_ARPG
             int tileValueDown = CollisionIDs[((int)player.PlayerPosition.X / 16 - 1), (((int)player.PlayerPosition.Y / 16 - 1)) + 1];
             if (tileValueDown == 1 || tileValueDown == 3 || tileValueDown == 5 || tileValueDown == 6 || tileValueDown == 7 || tileValueDown == 8 || tileValueDown == 9
                 || tileValueDown == 10 || tileValueDown == 11 || tileValueDown == 12 || tileValueDown == 14 || tileValueDown == 15 || tileValueDown == 16 || tileValueDown == 17
-                || tileValueDown == 19 || tileValueDown == 21 || tileValueDown == 22 || tileValueDown == 24 || tileValueDown == 29 || tileValueDown == 30 || tileValueDown == 31 || tileValueDown == 32
+                || tileValueDown == 19 || tileValueDown == 21 || tileValueDown == 22 || tileValueDown == 24 || tileValueDown == 28 || tileValueDown == 29 || tileValueDown == 30 || tileValueDown == 31 || tileValueDown == 32
                 || tileValueDown == 33 || tileValueDown == 34 || tileValueDown == 35 || tileValueDown == 36)
                 return true;
             return false;
@@ -168,7 +172,7 @@ namespace _2D_ARPG
             int tileValueLeft = CollisionIDs[(((int)player.PlayerPosition.X / 16 - 1)) - 1, (((int)player.PlayerPosition.Y / 16 - 1))];
             if (tileValueLeft == 1 || tileValueLeft == 3 || tileValueLeft == 5 || tileValueLeft == 6 || tileValueLeft == 7 || tileValueLeft == 8 || tileValueLeft == 9
                 || tileValueLeft == 10 || tileValueLeft == 11 || tileValueLeft == 12 || tileValueLeft == 14 || tileValueLeft == 15 || tileValueLeft == 16 || tileValueLeft == 17
-                || tileValueLeft == 19 || tileValueLeft == 21 || tileValueLeft == 22 || tileValueLeft == 24 || tileValueLeft == 29 || tileValueLeft == 30 || tileValueLeft == 31 || tileValueLeft == 32
+                || tileValueLeft == 19 || tileValueLeft == 21 || tileValueLeft == 22 || tileValueLeft == 24 || tileValueLeft == 28 || tileValueLeft == 29 || tileValueLeft == 30 || tileValueLeft == 31 || tileValueLeft == 32
                 || tileValueLeft == 33 || tileValueLeft == 34 || tileValueLeft == 35 || tileValueLeft == 36)
                 return true;
             return false;
@@ -179,7 +183,7 @@ namespace _2D_ARPG
             int tileValueRight = CollisionIDs[(((int)player.PlayerPosition.X / 16 - 1)) + 1, ((int)player.PlayerPosition.Y / 16 - 1)];
             if (tileValueRight == 1 || tileValueRight == 3 || tileValueRight == 5 || tileValueRight == 6 || tileValueRight == 7 || tileValueRight == 8 || tileValueRight == 9
                 || tileValueRight == 10 || tileValueRight == 11 || tileValueRight == 12 || tileValueRight == 14 || tileValueRight == 15 || tileValueRight == 16 || tileValueRight == 17
-                || tileValueRight == 19 || tileValueRight == 21 || tileValueRight == 22 || tileValueRight == 24 || tileValueRight == 29 || tileValueRight == 30 || tileValueRight == 31 || tileValueRight == 32
+                || tileValueRight == 19 || tileValueRight == 21 || tileValueRight == 22 || tileValueRight == 24 || tileValueRight == 28 || tileValueRight == 29 || tileValueRight == 30 || tileValueRight == 31 || tileValueRight == 32
                 || tileValueRight == 33 || tileValueRight == 34 || tileValueRight == 35 || tileValueRight == 36)
                 return true;
             return false;
@@ -189,7 +193,7 @@ namespace _2D_ARPG
         private void UpdatePlayer(GameTime gameTime)
         {
             player.Update(gameTime);
-            if (worldmap == 1)
+            if (worldMap == 1)
             {
                 if (currentKeyboardState.IsKeyDown(Keys.A) && currentKeyboardState.IsKeyUp(Keys.W) && currentKeyboardState.IsKeyUp(Keys.S)
                 && currentKeyboardState.IsKeyUp(Keys.D))
@@ -247,7 +251,7 @@ namespace _2D_ARPG
 
             if (KeyPressed(Keys.Enter))                    //sets worldmap active
             {
-                worldmap = 1;
+                worldMap = 1;
             }
         }
 
@@ -258,7 +262,7 @@ namespace _2D_ARPG
             spriteBatch.Begin(this.camera, SpriteSortMode.Deferred,
             BlendState.AlphaBlend, SamplerState.PointClamp);
             // Draw WorldMap
-            if (worldmap == 1)
+            if (worldMap == 1)
             {
                 foreach (Tile tile in tileset)
                 {
