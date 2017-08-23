9c06dcb72f4d2d52b695dc069ef1d84a8cfce33b
Sumppa <asd@asdasdasdasd.fi>
2017-08-23 22:22:47 +0300

    adding stairs as walkable tiles

--- 2D-ARPG/Game/Game1.cs
+++ 2D-ARPG/Game/Game1.cs
@@ -115,7 +115,7 @@ namespace _2D_ARPG
             if (tileValueUp == 1 || tileValueUp == 3 || tileValueUp == 5 || tileValueUp == 6 || tileValueUp == 7 || tileValueUp == 8 || tileValueUp == 9
                 || tileValueUp == 10 || tileValueUp == 11 || tileValueUp == 12 || tileValueUp == 14 || tileValueUp == 15 || tileValueUp == 16 || tileValueUp == 17
                 || tileValueUp == 19 || tileValueUp == 21 || tileValueUp == 22 || tileValueUp == 24 || tileValueUp == 28 || tileValueUp == 29 || tileValueUp == 30 || tileValueUp == 31 || tileValueUp == 32
-                || tileValueUp == 33 || tileValueUp == 34 || tileValueUp == 35 || tileValueUp == 36 || tileValueUp == 40)
+                || tileValueUp == 33 || tileValueUp == 34 || tileValueUp == 35 || tileValueUp == 36 || tileValueUp == 40 || tileValueUp == 46 || tileValueUp == 47)
                 return true;
             return false;
         }
@@ -126,7 +126,7 @@ namespace _2D_ARPG
             if (tileValueDown == 1 || tileValueDown == 3 || tileValueDown == 5 || tileValueDown == 6 || tileValueDown == 7 || tileValueDown == 8 || tileValueDown == 9
                 || tileValueDown == 10 || tileValueDown == 11 || tileValueDown == 12 || tileValueDown == 14 || tileValueDown == 15 || tileValueDown == 16 || tileValueDown == 17
                 || tileValueDown == 19 || tileValueDown == 21 || tileValueDown == 22 || tileValueDown == 24 || tileValueDown == 28 || tileValueDown == 29 || tileValueDown == 30 || tileValueDown == 31 || tileValueDown == 32
-                || tileValueDown == 33 || tileValueDown == 34 || tileValueDown == 35 || tileValueDown == 36 || tileValueDown == 40)
+                || tileValueDown == 33 || tileValueDown == 34 || tileValueDown == 35 || tileValueDown == 36 || tileValueDown == 40 || tileValueDown == 46 || tileValueDown == 47)
                 return true;
             return false;
         }
@@ -137,7 +137,7 @@ namespace _2D_ARPG
             if (tileValueLeft == 1 || tileValueLeft == 3 || tileValueLeft == 5 || tileValueLeft == 6 || tileValueLeft == 7 || tileValueLeft == 8 || tileValueLeft == 9
                 || tileValueLeft == 10 || tileValueLeft == 11 || tileValueLeft == 12 || tileValueLeft == 14 || tileValueLeft == 15 || tileValueLeft == 16 || tileValueLeft == 17
                 || tileValueLeft == 19 || tileValueLeft == 21 || tileValueLeft == 22 || tileValueLeft == 24 || tileValueLeft == 28 || tileValueLeft == 29 || tileValueLeft == 30 || tileValueLeft == 31 || tileValueLeft == 32
-                || tileValueLeft == 33 || tileValueLeft == 34 || tileValueLeft == 35 || tileValueLeft == 36 || tileValueLeft == 40)
+                || tileValueLeft == 33 || tileValueLeft == 34 || tileValueLeft == 35 || tileValueLeft == 36 || tileValueLeft == 40 || tileValueLeft == 46 || tileValueLeft == 47)
                 return true;
             return false;
         }
@@ -148,7 +148,7 @@ namespace _2D_ARPG
             if (tileValueRight == 1 || tileValueRight == 3 || tileValueRight == 5 || tileValueRight == 6 || tileValueRight == 7 || tileValueRight == 8 || tileValueRight == 9
                 || tileValueRight == 10 || tileValueRight == 11 || tileValueRight == 12 || tileValueRight == 14 || tileValueRight == 15 || tileValueRight == 16 || tileValueRight == 17
                 || tileValueRight == 19 || tileValueRight == 21 || tileValueRight == 22 || tileValueRight == 24 || tileValueRight == 28 || tileValueRight == 29 || tileValueRight == 30 || tileValueRight == 31 || tileValueRight == 32
-                || tileValueRight == 33 || tileValueRight == 34 || tileValueRight == 35 || tileValueRight == 36 || tileValueRight == 40)
+                || tileValueRight == 33 || tileValueRight == 34 || tileValueRight == 35 || tileValueRight == 36 || tileValueRight == 40 || tileValueRight == 46 || tileValueRight == 47)
                 return true;
             return false;
         }
