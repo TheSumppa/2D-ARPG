b3728ab853445b645cf313ebe048f09d5848b4d9
Sumppa <asd@asdasdasdasd.fi>
2017-08-25 22:24:11 +0300

    Adjusting camera pos, new tiles, new map file

--- 2D-ARPG/Game/Game1.cs
+++ 2D-ARPG/Game/Game1.cs
@@ -70,14 +70,14 @@ namespace _2D_ARPG
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
-            spriteBatch = new SpriteBatch(GraphicsDevice);                
+            spriteBatch = new SpriteBatch(GraphicsDevice);
             WalkAnimation playerAnimation = new WalkAnimation();
             Texture2D playerTexture = Content.Load<Texture2D>("knightwalkanimation");
             Vector2 playerPosition = new Vector2(880, 768);
             Rectangle playerRectangle = new Rectangle((int)playerPosition.X, (int)playerPosition.Y, 16, 16);
             playerAnimation.Initialize(playerTexture, playerPosition, 16, 16, 2, 200, Color.White, 1.0f, true);
             player.Initialize(playerAnimation, playerPosition);
-            camera.Scale = 5.0f;
+            camera.Scale = 6.0f;
         }
 
         /// UnloadContent will be called once per game and is the place to unload
@@ -94,13 +94,13 @@ namespace _2D_ARPG
         {
             if (Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-            
+            Vector2 PlayerOffset = new Vector2(player.PlayerPosition.X - 8, player.PlayerPosition.Y - 8);
             float seconds = (float)gameTime.ElapsedGameTime.TotalSeconds;
             previousKeyboardState = currentKeyboardState;
             currentKeyboardState = Keyboard.GetState();
             elapsedTime = seconds;
-            this.camera.Update(gameTime);
-            this.camera.Position = player.PlayerPosition;
+            this.camera.Update(gameTime);  
+            this.camera.Position = PlayerOffset;
             checkTileValue();
             UpdatePlayer(gameTime);
             Debug.WriteLine("PosX: " + player.PlayerPosition.X);
@@ -122,7 +122,7 @@ namespace _2D_ARPG
             if (tileValueUp == 1 || tileValueUp == 3 || tileValueUp == 5 || tileValueUp == 6 || tileValueUp == 7 || tileValueUp == 8 || tileValueUp == 9
                 || tileValueUp == 10 || tileValueUp == 11 || tileValueUp == 12 || tileValueUp == 14 || tileValueUp == 15 || tileValueUp == 16 || tileValueUp == 17
                 || tileValueUp == 19 || tileValueUp == 21 || tileValueUp == 22 || tileValueUp == 24 || tileValueUp == 28 || tileValueUp == 29 || tileValueUp == 30 || tileValueUp == 31 || tileValueUp == 32
-                || tileValueUp == 33 || tileValueUp == 34 || tileValueUp == 35 || tileValueUp == 36 || tileValueUp == 39 || tileValueUp == 40 || tileValueUp == 46 || tileValueUp == 47)
+                || tileValueUp == 33 || tileValueUp == 34 || tileValueUp == 35 || tileValueUp == 36 || tileValueUp == 39 || tileValueUp == 40 || tileValueUp == 46 || tileValueUp == 47 || tileValueUp == 48)
                 return true;
             return false;
         }
@@ -133,7 +133,7 @@ namespace _2D_ARPG
             if (tileValueDown == 1 || tileValueDown == 3 || tileValueDown == 5 || tileValueDown == 6 || tileValueDown == 7 || tileValueDown == 8 || tileValueDown == 9
                 || tileValueDown == 10 || tileValueDown == 11 || tileValueDown == 12 || tileValueDown == 14 || tileValueDown == 15 || tileValueDown == 16 || tileValueDown == 17
                 || tileValueDown == 19 || tileValueDown == 21 || tileValueDown == 22 || tileValueDown == 24 || tileValueDown == 28 || tileValueDown == 29 || tileValueDown == 30 || tileValueDown == 31 || tileValueDown == 32
-                || tileValueDown == 33 || tileValueDown == 34 || tileValueDown == 35 || tileValueDown == 36 || tileValueDown == 39 || tileValueDown == 40 || tileValueDown == 46 || tileValueDown == 47)
+                || tileValueDown == 33 || tileValueDown == 34 || tileValueDown == 35 || tileValueDown == 36 || tileValueDown == 39 || tileValueDown == 40 || tileValueDown == 46 || tileValueDown == 47 || tileValueDown == 48)
                 return true;
             return false;
         }
@@ -144,7 +144,7 @@ namespace _2D_ARPG
             if (tileValueLeft == 1 || tileValueLeft == 3 || tileValueLeft == 5 || tileValueLeft == 6 || tileValueLeft == 7 || tileValueLeft == 8 || tileValueLeft == 9
                 || tileValueLeft == 10 || tileValueLeft == 11 || tileValueLeft == 12 || tileValueLeft == 14 || tileValueLeft == 15 || tileValueLeft == 16 || tileValueLeft == 17
                 || tileValueLeft == 19 || tileValueLeft == 21 || tileValueLeft == 22 || tileValueLeft == 24 || tileValueLeft == 28 || tileValueLeft == 29 || tileValueLeft == 30 || tileValueLeft == 31 || tileValueLeft == 32
-                || tileValueLeft == 33 || tileValueLeft == 34 || tileValueLeft == 35 || tileValueLeft == 36 || tileValueLeft == 39 || tileValueLeft == 40 || tileValueLeft == 46 || tileValueLeft == 47)
+                || tileValueLeft == 33 || tileValueLeft == 34 || tileValueLeft == 35 || tileValueLeft == 36 || tileValueLeft == 39 || tileValueLeft == 40 || tileValueLeft == 46 || tileValueLeft == 47 || tileValueLeft == 48)
                 return true;
             return false;
         }
@@ -155,7 +155,7 @@ namespace _2D_ARPG
             if (tileValueRight == 1 || tileValueRight == 3 || tileValueRight == 5 || tileValueRight == 6 || tileValueRight == 7 || tileValueRight == 8 || tileValueRight == 9
                 || tileValueRight == 10 || tileValueRight == 11 || tileValueRight == 12 || tileValueRight == 14 || tileValueRight == 15 || tileValueRight == 16 || tileValueRight == 17
                 || tileValueRight == 19 || tileValueRight == 21 || tileValueRight == 22 || tileValueRight == 24 || tileValueRight == 28 || tileValueRight == 29 || tileValueRight == 30 || tileValueRight == 31 || tileValueRight == 32
-                || tileValueRight == 33 || tileValueRight == 34 || tileValueRight == 35 || tileValueRight == 36 || tileValueRight == 39 || tileValueRight == 40 || tileValueRight == 46 || tileValueRight == 47)
+                || tileValueRight == 33 || tileValueRight == 34 || tileValueRight == 35 || tileValueRight == 36 || tileValueRight == 39 || tileValueRight == 40 || tileValueRight == 46 || tileValueRight == 47 || tileValueRight == 48)
                 return true;
             return false;
         }
@@ -231,15 +231,15 @@ namespace _2D_ARPG
                 currentCollisionIDs = TownCollisionIDs;
                 player.PlayerPosition.X = 592;
                 player.PlayerPosition.Y = 720;
-                             
+
             }
-            if(currentTileValue == 39)                      // Check if player want's to leave a town1 and set correct tileSet, collisionIDS and player positionv
+            if (currentTileValue == 39)                      // Check if player want's to leave a town1 and set correct tileSet, collisionIDS and player positionv
             {
                 currentTileset = tilesetWorldMap;
                 currentCollisionIDs = mapCollisionIDs;
                 player.PlayerPosition.X = 880;
                 player.PlayerPosition.Y = 768;
-                
+
             }
         }
 
@@ -249,14 +249,12 @@ namespace _2D_ARPG
             GraphicsDevice.Clear(Color.CornflowerBlue);
             spriteBatch.Begin(this.camera, SpriteSortMode.Deferred,
             BlendState.AlphaBlend, SamplerState.PointClamp);
-            // Draw WorldMap
-                foreach (Tile tile in currentTileset)
-                {
-                    tile.Draw(spriteBatch);
-                    // Draw Player
-                    player.Draw(spriteBatch);
-                }  
-
+            // Draw WorldMap           
+            foreach (Tile tile in currentTileset)
+            {
+                tile.Draw(spriteBatch);
+            }
+            player.Draw(spriteBatch);
             spriteBatch.End();
             base.Draw(gameTime);
         }
